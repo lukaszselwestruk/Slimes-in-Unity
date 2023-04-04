911b9809c5fc11bfaeb729d94d815cb25d55beec
lukasz.selwestruk@gmail.com <>
2023-04-04 22:59:19 +0200

    Add animator, animation for player sprite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,11 +13,15 @@ using UnityEngine;
         private PlayerActions _playerActions;
         private Vector2 _movement;
         private Rigidbody2D _rb;
+        private Animator _myAnimator;
+        private SpriteRenderer _mySpriteRenderer;
 
         private void Awake()
         {
             _playerActions = new PlayerActions();
             _rb = GetComponent<Rigidbody2D>();
+            _myAnimator = GetComponent<Animator>();
+            _mySpriteRenderer = GetComponent<SpriteRenderer>();
         }
         // new input system requires this method.
         private void OnEnable()
@@ -28,6 +32,8 @@ using UnityEngine;
         private void PlayerInput()
         {
             _movement = _playerActions.Movement.Move.ReadValue<Vector2>();
+            _myAnimator.SetFloat("moveX", _movement.x);
+            _myAnimator.SetFloat("moveY", _movement.y);
         }
 
         private void Move()
@@ -41,7 +47,17 @@ using UnityEngine;
 
         private void FixedUpdate() 
         {
+            SetPlayerFacingDirection();
             Move();
+        }
+
+        private void SetPlayerFacingDirection()
+        {
+            var mousePos = Input.mousePosition;
+            var playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            if (mousePos.x < playerScreenPoint.x) _mySpriteRenderer.flipX = true;
+            else _mySpriteRenderer.flipX = false;
+
         }
     }
 
