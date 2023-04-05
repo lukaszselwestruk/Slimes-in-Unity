e37f952238fec58eb777bb3ba2421d47aea5dd12
lukasz.selwestruk@gmail.com <>
2023-04-05 22:19:33 +0200

    Fix player animation

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -7,9 +7,13 @@ public class Sword : MonoBehaviour
 {
     private PlayerActions playerActions;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake()
     {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
         playerActions = new PlayerActions();
     }
@@ -26,5 +30,25 @@ public class Sword : MonoBehaviour
     private void Attack()
     {
         myAnimator.SetTrigger("Attack");
+    }
+    private void Update()
+    {
+        mouseFollowWithOffset();
+    }
+    private void mouseFollowWithOffset()
+    {
+        var mousePos = Input.mousePosition;
+        var playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        var angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;     
+        
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
